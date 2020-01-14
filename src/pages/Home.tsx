733f3abc1fe38bfbf5fbf32b1f4145e2a5bc7428
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-01-14 23:54:17 +0100

    Add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+export const Home: React.FC = () => {
+  return (
+    <Fragment>
+      <h1>Redux + TypeScript</h1>
+      <p>
+        An opinionated Create React App template with React Router, TypeScript
+        and Redux
+      </p>
+    </Fragment>
+  )
+}
