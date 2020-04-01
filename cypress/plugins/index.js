d4bd45a5742a9a50b273deb2798491bca7f2a3c8
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-04-01 18:25:31 +0200

    chore: ignore some lint errors in the cypress/plugin file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line spaced-comment
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
