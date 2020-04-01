2acbb194b9e23511e0d76048b42d7fb018621131
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-04-01 18:46:47 +0200

    chore: add e2e tests for the about page

---
+++ cypress/integration/about.spec.js
@@ -0,0 +1,17 @@
+describe('About page', () => {
+  beforeEach(() => {
+    cy.visit('/about')
+  })
+
+  it('should navigate to the page and go back via "Go back" button', () => {
+    cy.get('h1').contains('About')
+    cy.get('[cy-data="go-back-button"]').click()
+    cy.get('h1').contains('Redux + TypeScript')
+  })
+
+  it('should navigate to the about page and back via header link', () => {
+    cy.get('h1').contains('About')
+    cy.get('[cy-data="home-nav-link"] > .active').click()
+    cy.get('h1').contains('Redux + TypeScript')
+  })
+})
