7469023f5ed3348271ef1051e10e5cd2ec8765f4
amanuel2 <amanuelb43@gmail.com>
2020-11-29 08:15:51 -0500

    Organized Folder Structure, and Added Redux-Logger

---
+++ template/src/redux/store.ts
@@ -0,0 +1,21 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
+import logger from 'redux-logger'
+import { CounterReducer } from './counter'
+
+/* Create root reducer, containing all features of the application */
+const rootReducer = combineReducers({
+    count: CounterReducer,
+})
+
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(logger),
+
+        /* preloadedState, */ devToolsEnhancer({})
+    )
+)
+
+export default store
