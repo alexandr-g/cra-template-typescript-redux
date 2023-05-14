746096b3443da25d444eb247bf25230ac37efa31
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2023-05-14 18:22:59 +0200

    feat: upgrade to react 18, react-router v6, react-redux v8

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
 import { About } from './pages/About'
@@ -10,10 +10,10 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Navbar />
       <div className="container">
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </BrowserRouter>
   )
